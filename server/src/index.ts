f0005c12b3d05f0f2c46cbe9a51dd997294c50d5
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-03-23 21:58:24 +1000

    feat(server): additional logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,29 @@ dotenv.config();
 
 import { Streamer } from 'hive-stream';
 
+import express from 'express';
+
 import config from './config';
 
 const streamer = new Streamer();
+
+const app = express();
+const port = 5001;
 
 streamer.start();
 
 streamer.onTransfer(config.WATCH_ACCOUNT, () => {
     
+});
+
+app.get('/', (req, res) => {
+    res.send('Invalid');
+});
+
+app.get('/pay/:from/:to', (req, res) => {
+
+});
+
+const server = app.listen(port, () => {
+    console.log(`Running server on port ${port}`);
 });

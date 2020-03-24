0013f6ea41bf4ed14f11031eb7b95af9196bc05c
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-03-24 23:09:35 +1000

    feat(tests): mocking fetch and packages

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,9 +1,15 @@
-(global as any).console = {
-    log: jest.fn(), // console.log are ignored in tests
+import { GlobalWithFetchMock } from 'jest-fetch-mock';
 
-    // Keep native behaviour for other methods, use those to print out things in your own tests, not `console.log`
-    error: console.error,
-    warn: console.warn,
-    info: console.info,
-    debug: console.debug,
-};
+const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
+customGlobal.fetch = require('jest-fetch-mock');
+customGlobal.fetchMock = customGlobal.fetch;;
+
+// (global as any).console = {
+//     log: jest.fn(), // console.log are ignored in tests
+
+//     // Keep native behaviour for other methods, use those to print out things in your own tests, not `console.log`
+//     error: console.error,
+//     warn: console.warn,
+//     info: console.info,
+//     debug: console.debug,
+// };

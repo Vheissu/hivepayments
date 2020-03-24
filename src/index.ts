f73a2ad0b576918e46d63039783cba1dace1eac9
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-03-24 16:37:41 +1000

    feat(api): get transfer history lookup

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,22 @@ app.get('/pay/:from/:to', (req, res) => {
 
 });
 
+app.get('/getTransfer/:id', async (req, res) => {
+    const client = streamer['client'];
+    const history = await client.call('database_api', 'get_account_history', [req.params.id, -1, 20]);
+    const transfers = history.filter(tx => tx[1].op[0] === 'transfer');
+
+    const actualTransfers = transfers.reduce((arr, tx) => {
+        arr.push(tx[1].op[1]);
+
+        return arr;
+    }, []);
+
+    console.log(actualTransfers);
+
+    res.send('Yeah');
+});
+
 const server = app.listen(port, () => {
     console.log(`Running server on port ${port}`);
 });

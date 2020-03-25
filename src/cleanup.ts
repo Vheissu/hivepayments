bb7cd86870d3a6af096c3dac680b042ac1d7c2c9
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-03-25 10:33:45 +1000

    feat(scripts): cleanup script for removing payments older than 1 hour

---
+++ src/cleanup.ts
@@ -0,0 +1,19 @@
+import { find, remove } from "./database";
+
+const ONE_HOUR = 1000 * 60 * 60;
+
+async function cleanupOldPayments() {
+    const HOUR_AGO = Date.now() - ONE_HOUR;
+
+    const payments = await find('payments', { status: 'new' });
+
+    if (payments?.length) {
+        for (const payment of payments) {
+            if (payment.date < HOUR_AGO) {
+                await remove('payments', { _id: payment._id });
+            }
+        }
+    }
+}
+
+cleanupOldPayments();
